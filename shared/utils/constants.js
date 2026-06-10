5af4292f7e08bd07a417750421a13837cda081fe
focushub360 <focushub360db@gmail.com>
2026-06-10 11:11:12 +0530

    Commit
    Updated - admin attendence report -> report -> table 1 to add global present/absent.

--- shared/utils/constants.js
+++ shared/utils/constants.js
@@ -1,6 +1,6 @@
 // During local development point to the local backend. Change back to production URL when deploying.
-// export const API_BASE_URL = 'http://localhost:5000/api';
- export const API_BASE_URL = 'https://hrm-backend-axau.onrender.com/api';
+//export const API_BASE_URL = 'http://localhost:5000/api';
+export const API_BASE_URL = 'https://hrm-backend-axau.onrender.com/api';
 
 // User Roles
 export const USER_ROLES = {
